abc2cab3a1366e3b6f49083a6fc44b0b60afa99b
Kingsley Ubah <ubahkingsley4@gmail.com>
2022-07-12 06:31:54 -0700

    corrections

--- NEWO.js
+++ NEWO.js
@@ -1,18 +1,21 @@
 const notes = [
     {
-        data: {content: 'Sticky note 1', shape: 'square'},
+        data: {content: 'Sticky note 4', shape: 'square'},
         style: {fillColor: 'light_yellow', textAlign: 'center', textAlignVertical: 'middle'},        
         geometry: {height: 150},
+        pos: {x: 0, y: 0},
     },
     {
-        data: {content: 'Sticky note 2', shape: 'square'},
+        data: {content: 'Sticky note 5', shape: 'square'},
         style: {fillColor: 'gray', textAlign: 'center', textAlignVertical: 'middle'},        
         geometry: {height: 150},
+        pos: {x: 150, y: 0},
     },
     {
-        data: {content: 'Sticky note 3', shape: 'square'},
+        data: {content: 'Sticky note 6', shape: 'square'},
         style: {fillColor: 'black', textAlign: 'center', textAlignVertical: 'middle'},        
         geometry: {height: 150},
+        pos: {x: 300, y: 0},
     },
 ]
 
---
+++ TESTO.js
@@ -0,0 +1,52 @@
+const notes = [   
+    {
+        data: {content: 'Sticky note 3', shape: 'square'},
+        style: {fillColor: 'orange', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 4', shape: 'square'},
+        style: {fillColor: 'pink', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 5', shape: 'square'},
+        style: {fillColor: 'violet', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 6', shape: 'square'},
+        style: {fillColor: 'green', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 7', shape: 'square'},
+        style: {fillColor: 'black', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 8', shape: 'square'},
+        style: {fillColor: 'dark_blue', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 9', shape: 'square'},
+        style: {fillColor: 'pink', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 10', shape: 'square'},
+        style: {fillColor: 'violet', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 11', shape: 'square'},
+        style: {fillColor: 'red', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    },
+    {
+        data: {content: 'Sticky note 12', shape: 'square'},
+        style: {fillColor: 'cyan', textAlign: 'center', textAlignVertical: 'middle'},        
+        geometry: {height: 150},
+    }
+]
